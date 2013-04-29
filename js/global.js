3a528a938bf76961195e1827d1a9c5f8c9ff1f79
Josh Reisner <josh@joshreisner.com>
2013-04-29 01:04:13 -0700

    fixing dragging, adding dice

--- js/global.js
+++ js/global.js
@@ -5,22 +5,33 @@ $(function(){
 
 	setDraggable();
 	
-	function setDraggable(){
-		$("div.piece.ui-draggable").each(function(){ 
-			$(this).draggable('disable'); 
-		});
-		$("div.point.player1").each(function(){
-			$(this).find("div.piece").last().draggable();
-		});
-	}
-	
-	$("div.point").droppable({ 
+	$("div.point:not(.player2)").droppable({ 
 		hoverClass: "hover-piece",
 		drop: function(event, ui) {
-			var piece = $(ui.draggable).attr("style", "").remove();
-			$(this).addClass("player1").prepend(piece);
+			var piece = $(ui.draggable).remove();
+			$(this).addClass("player1").prepend('<div class="piece"></div>');
+
 			setDraggable();
 		}
 	});
+	
+	//roll dice
+	$("div.die").click(function(){
+		$("div.die div.face").removeClass("active");
+		for (var i = 1; i < 3; i++) {
+			var roll = Math.floor(Math.random() * 6) + 1;
+			$("div#die" + i).find("div.face[data-id=" + roll + "]").addClass("active");
+		}
+	});
+	
+	function setDraggable() {
+		$("div.piece.ui-draggable").each(function(){ 
+			$(this).draggable('destroy'); 
+		});
+
+		$("div.point.player1").each(function(){
+			$(this).find("div.piece").last().draggable({revert: "invalid"});
+		});
+	}
 
 });
